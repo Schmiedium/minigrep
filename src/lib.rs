70ca4a094a28cd9e15f9a7c7fc488c564efe428a
Eisenschmied <1591704563121002@mil>
2022-08-10 18:02:04 -0400

    working through chapter 12 in the rust book completed 12.3

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use std::{fs, error::Error};
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text: \n{}", contents);
+    Ok(())
+}
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[2].clone();
+        let filename = args[3].clone();
+
+        return Ok(Config { query, filename});
+    }
+}
