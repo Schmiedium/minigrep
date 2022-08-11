9f3dd220c3af6d0c5b474b623fc80da77aed3736
Schmiedium <alexander.eisenschmied@gmail.com>
2022-08-10 22:14:43 -0400

    changed array indices in Config for linux got through 12.4, program works

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,24 @@
 use std::{fs, error::Error};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
+    let contents: String = fs::read_to_string(config.filename)?;
 
-    println!("With text: \n{}", contents);
+    for line in search(&config.query, &contents){
+        println!("{}", line);
+    }
+
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results: Vec<&str> = Vec::new();
+    contents.lines().for_each(|line: &str| {
+        if line.contains(query) {
+            results.push(line);
+        }
+    });
+
+    results
 }
 pub struct Config {
     pub query: String,
@@ -17,9 +31,24 @@ impl Config {
             return Err("not enough arguments");
         }
 
-        let query = args[2].clone();
-        let filename = args[3].clone();
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
         return Ok(Config { query, filename});
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, productive, fast.
+Pick Three.";
+        assert_eq!(vec!["safe, productive, fast."], search(query, contents));
     }
 }
